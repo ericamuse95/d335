ec0c672c587d7832c65dd3afebd87af040bd8d8c
Erica Muse <ericavmuse@gmail.com>
2025-03-05 04:53:52 -0800

    Add files via upload
    lab332 complete

---
+++ lab332.py
@@ -0,0 +1,12 @@
+#Complete a program that reads four values from input
+#and stores the values in variables:
+#first_name, generic_location, whole_number, and plural_noun.
+#The program then uses the input values to output a short story.
+#The first input statement is provided in the code as an example.
+
+first_name = input("First name:")
+generic_location = input("Location:")
+whole_number = int(input("Number:"))
+plural_noun = input("Plural noun:")
+
+print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
